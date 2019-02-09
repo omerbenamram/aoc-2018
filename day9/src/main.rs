1a0ea280400d3937cf46753db3c11f1353bebf46
Omer Ben-Amram <omerbenamram@gmail.com>
2019-02-09 20:01:53 +0200

    second attempt - vec based linked list

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -8,22 +8,42 @@ macro_rules! err {
     ($($tt:tt)*) => { Err(Box::<Error>::from(format!($($tt)*))) }
 }
 
-type Marble = u32;
 type PlayerId = u32;
 type Score = u32;
 
+type MarbleValue = u32;
+type MarbleId = usize;
+
 struct MarblesGame {
-    board: LinkedList<Marble>,
+    board: Vec<Marble>,
     scores: HashMap<PlayerId, Score>,
     n_players: u32,
     current_player: PlayerId,
-    current_marble: usize,
+    // Position in the vector of the current marble
+    current_marble: MarbleId,
+}
+
+#[derive(Clone)]
+struct Marble {
+    value: MarbleValue,
+    previous_id: MarbleId,
+    next_id: MarbleId,
+}
+
+impl Marble {
+    pub fn with_value(value: u32) -> Self {
+        Marble {
+            value,
+            previous_id: 0,
+            next_id: 0,
+        }
+    }
 }
 
 impl MarblesGame {
-    pub fn new(n_players: u32) -> Self {
-        let mut initial_board = LinkedList::new();
-        initial_board.push_back(0);
+    pub fn new(n_players: u32, max_marble_value: usize) -> Self {
+        let mut initial_board = Vec::with_capacity(max_marble_value);
+        initial_board.push(Marble::with_value(0));
 
         let mut initial_scores = HashMap::new();
 
@@ -40,39 +60,61 @@ impl MarblesGame {
         }
     }
 
+    fn add_marble_to_board(&mut self, marble: MarbleValue) -> MarbleId {
+        let marble = Marble::with_value(marble);
+        self.board.push(marble);
+        self.board.len() - 1
+    }
+
     /// Insert marble into board without copying all data.
-    fn insert_at(&mut self, idx: usize, marble: Marble) {
-        let right = self.board.split_off(idx);
-        self.board.push_back(marble);
-        self.board.extend(right);
+    fn insert_after(&mut self, idx: MarbleId, marble: MarbleValue) -> MarbleId {
+        let old_next = self.board[idx].next_id;
+
+        let my_idx = self.add_marble_to_board(marble);
+        self.board[my_idx].previous_id = idx;
+        self.board[my_idx].next_id = old_next;
+
+        self.board[idx].next_id = my_idx;
+        self.board[old_next].previous_id = my_idx;
+        my_idx
     }
 
-    fn remove_at(&mut self, idx: usize) -> Marble {
-        let right = self.board.split_off(idx + 1);
-        let marble = self
-            .board
-            .pop_back()
-            .unwrap_or_else(|| panic!("A marble should be present at idx {}", idx));
+    fn clockwise(&self, amount: usize) -> MarbleId {
+        let mut current = self.current_marble;
 
-        self.board.extend(right);
-        marble
+        for i in 1..=amount {
+            current = self.board[current].next_id
+        }
+
+        current
     }
 
-    fn add_regular_marble(&mut self, marble: Marble) {
-        let last_marble_idx = self.board.len() - 1;
+    fn counter_clockwise(&self, amount: usize) -> MarbleId {
+        let mut current = self.current_marble;
 
-        // If current marble is last, we start from front of list
-        let marble_position: usize = if self.current_marble == last_marble_idx {
-            1
-        } else {
-            self.current_marble + 2
-        };
+        for i in 1..=amount {
+            current = self.board[current].previous_id
+        }
 
-        self.insert_at(marble_position, marble);
-        self.current_marble = marble_position;
+        current
     }
 
-    fn add_special_marble(&mut self, marble: Marble) {
+    fn remove_at(&mut self, idx: MarbleId) -> MarbleValue {
+        let prev = self.board[idx].previous_id;
+        let next = self.board[idx].next_id;
+
+        self.board[prev].next_id = next;
+        self.board[next].previous_id = prev;
+
+        self.board[idx].value
+    }
+
+    fn add_regular_marble(&mut self, marble: MarbleValue) {
+        let current_marble = self.insert_after(self.clockwise(1), marble);
+        self.current_marble = current_marble;
+    }
+
+    fn add_special_marble(&mut self, marble: MarbleValue) {
         // First, the current player keeps the marble they would have placed,
         // adding it to their score.
         self.scores
@@ -81,22 +123,16 @@ impl MarblesGame {
 
         // In addition, the marble 7 marbles counter-clockwise from the current marble
         // is removed from the circle and also added to the current player's score.
-        let next_marble_to_remove = if self.current_marble.saturating_sub(7) == 0 {
-            let offset_from_front = 7 - self.current_marble;
-            self.board.len() - offset_from_front
-        } else {
-            self.current_marble - 7
-        };
+        let next_marble_to_remove = self.counter_clockwise(7);
+        self.current_marble = self.board[next_marble_to_remove].next_id;
 
         let additional_marble = self.remove_at(next_marble_to_remove);
         self.scores
             .entry(self.current_player)
             .and_modify(|score| *score += additional_marble);
-
-        self.current_marble = next_marble_to_remove;
     }
 
-    pub fn add_marble(&mut self, marble: Marble) {
+    pub fn add_marble(&mut self, marble: u32) {
         let marble_is_special = marble % 23 == 0;
 
         if marble_is_special {
@@ -122,24 +158,40 @@ impl MarblesGame {
 }
 
 fn marble_game(num_players: u32, n_marbles: u32) -> Score {
-    let mut game = MarblesGame::new(num_players);
+    let mut game = MarblesGame::new(num_players, n_marbles as usize);
 
     for marble in 1..=n_marbles {
         game.add_marble(marble);
-        // println!("{}", game);
     }
 
     game.high_score()
 }
 
+impl Display for Marble {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::result::Result<(), std::fmt::Error> {
+        write!(f, "{}", self.value)?;
+        Ok(())
+    }
+}
+
 impl Display for MarblesGame {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::result::Result<(), std::fmt::Error> {
         write!(f, "[{}]", self.current_player)?;
-        for (i, marble) in self.board.iter().enumerate() {
-            if i == self.current_marble {
-                write!(f, " ({}) ", marble)?;
+        let mut marble = self.board[0].clone();
+        write!(f, " {} ", marble.value)?;
+
+        loop {
+            let next = marble.next_id;
+            marble = self.board[next].clone();
+
+            if next == 0 {
+                break;
+            }
+
+            if next == self.current_marble {
+                write!(f, " ({}) ", marble.value)?;
             } else {
-                write!(f, " {} ", marble)?;
+                write!(f, " {} ", marble.value)?;
             }
         }
 
